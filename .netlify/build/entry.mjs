11583ec146bf19339fde4c090b8ad13a913fdde9
t-a-k-a-dot <simp1e.lab.adm@gmail.com>
2025-06-28 03:25:42 +0900

    move og-image.jpg

--- .netlify/build/entry.mjs
+++ .netlify/build/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_CvSoi7hX.mjs';
-import { manifest } from './manifest_CR54BCQw.mjs';
+import { manifest } from './manifest_C8mWKBzB.mjs';
 import { createExports } from '@astrojs/netlify/ssr-function.js';
 
 const serverIslandMap = new Map();;
@@ -32,7 +32,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "1312a08b-5eaa-4392-8a4f-cad3f2b711f0"
+    "middlewareSecret": "5aff3007-4470-4008-b1ed-a50b46dac568"
 };
 const _exports = createExports(_manifest, _args);
 const __astrojsSsrVirtualEntry = _exports.default;
